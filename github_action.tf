756b55c199d7f5a99fa72a6c0806ff7ad680b83d
Jeffrey Monte <46807283+jmonte-sph@users.noreply.github.com>
2023-04-17 10:25:06 +0800

    fix the oicd provider and github repo name

--- github_action.tf
+++ github_action.tf
@@ -11,7 +11,7 @@ module "lamda_gha" {
 
   count = var.create_github_actions_role ? 1 : 0
 
-  openid_connect_provider_arn = var.create_github_actions_oidc_provider ? module.oidc_provider.openid_connect_provider.arn : data.aws_iam_openid_connect_provider.github.arn
+  openid_connect_provider_arn = var.create_github_actions_oidc_provider ? module.oidc_provider[0].openid_connect_provider.arn : data.aws_iam_openid_connect_provider.github.arn
   repo                        = var.github_repo.repo
   role_name                   = var.github_repo.role_name
   github_environments         = var.github_repo.environments
@@ -22,7 +22,7 @@ module "lamda_gha" {
     {
       test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
-      values   = [for branch in var.github_repo.branches : "repo:${each.value.repo}:ref:refs/heads/${branch}"]
+      values   = [for branch in var.github_repo.branches : "repo:${var.github_repo.repo}:ref:refs/heads/${branch}"]
     },
   ] : []
 }
