6684071b4450b296fb202dcf4049068b5f68a6ec
Goh Hong Yi <hygoh@sph.com.sg>
2024-02-19 14:58:28 +0800

    fix condition

--- github_action.tf
+++ github_action.tf
@@ -18,11 +18,13 @@ module "lambda_gha" {
 
   default_conditions = var.default_conditions
 
-  conditions = length(var.github_repo.branches) > 0 || length(var.github_repo.tags) > 0 ? {
-    test     = "StringLike"
-    variable = "token.actions.githubusercontent.com:sub"
-    values   = compact(concat([for branch in var.github_repo.branches : "repo:${var.github_repo.repo}:ref:refs/heads/${branch}"], [for tag in var.github_repo.tags : "repo:${var.github_repo.repo}:ref:refs/tags/${tag}"]))
-  } : []
+  conditions = length(var.github_repo.branches) > 0 || length(var.github_repo.tags) > 0 ? [
+    {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = compact(concat([for branch in var.github_repo.branches : "repo:${var.github_repo.repo}:ref:refs/heads/${branch}"], [for tag in var.github_repo.tags : "repo:${var.github_repo.repo}:ref:refs/tags/${tag}"]))
+    }
+  ] : []
 }
 
 moved {
