5df8b39319449f8d879c1c10a09ea7623b244025
tymnicholas <46499723+tymnicholas@users.noreply.github.com>
2025-07-28 14:35:47 +0800

    Update data.tf for resource regex

--- data.tf
+++ data.tf
@@ -65,7 +65,7 @@ data "aws_iam_policy_document" "update_lambda" {
       actions = [
         "lambda:UpdateEventSourceMapping",
       ]
-      resources = ["arn:aws:lambda:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:function:${var.function_prefix}*"]
+      resources = ["arn:aws:lambda:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:event-source-mapping:*"]
     }
   }
 }
